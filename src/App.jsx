506f67bf3e12b78d63d8d049ec4424ec89a6fae0
SARAVANAKANTH <128817809+SARAVANAKANTH16@users.noreply.github.com>
2024-05-28 16:05:22 +0530

    Add files via upload

---
+++ src/App.jsx
@@ -0,0 +1,18 @@
+import React, { useState } from 'react'
+import Navbar from './assets/Components/Navbar'
+import NewsBoard from './assets/Components/NewsBoard'
+
+const App = () => {
+
+  const [category,setCategory]=useState("general");
+
+  return (
+    <div>
+      <Navbar setCategory={setCategory}/>
+      <NewsBoard category={category}/>
+      
+    </div>
+  )
+}
+
+export default App
